a08f168eb7ad2dca91a244a938528f7741cd38c4
GASTONE DERECK ALEX <gastonderick@yahoo.com>
2022-01-05 11:17:57 +0300

    Add files via upload

---
+++ day 0.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
+	fmt.Println("Hello, World.")
+	fmt.Println(text)
+
+}
