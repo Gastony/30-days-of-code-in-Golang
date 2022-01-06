d399c08095aa94554b7c93a600d694ce5c863a62
GASTONE DERECK ALEX <gastonderick@yahoo.com>
2022-01-06 14:35:59 +0300

    Add files via upload

---
+++ day 1.go
@@ -0,0 +1,14 @@
+
+    
+var (
+	num uint64
+	b   float64
+	c   string
+)
+fmt.Scanln(&num)
+fmt.Scanf("%f", &b)
+scanner.Scan()
+c = scanner.Text()
+fmt.Println(num + i)
+fmt.Printf("%.1f\n", d+b)
+fmt.Print(s + c)
